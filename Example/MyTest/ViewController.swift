287d821e1fe3482f68005599ab0a80ad11f55f1e
yanglupeng <961505161@qq.com>
2017-08-16 08:55:08 +0800

    初始化

---
+++ Example/MyTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyTest
+//
+//  Created by Tony on 2017/8/16.
+//  Copyright © 2017年 Tony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let redView = RedView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        view.addSubview(redView)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MyTest.podspec
@@ -0,0 +1,20 @@
+Pod::Spec.new do |s|
+
+  s.name         = "MyTest"
+  s.version      = "1.0.0"
+  s.summary      = "简要介绍..."
+  s.description  = "描述内容..."
+  
+  s.homepage     = "https://github.com/splsylp/MyTest"
+  s.license      = "MIT"
+  s.author       = { "yanglupeng" => "961505161@qq.com" }
+
+  s.platform     = :ios, "8.0"
+  s.source       = { :git => "https://github.com/splsylp/MyTest.git", :tag => "1.0.0" }
+  s.source_files  = "Source/*.swift"
+  
+  s.frameworks = "UIKit", "QuartzCore"
+  s.requires_arc = true
+  s.dependency "SDWebImage"
+
+end
